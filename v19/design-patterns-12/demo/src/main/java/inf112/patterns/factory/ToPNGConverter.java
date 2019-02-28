d04c6b99ceccd5adb891d08160f712a868203657
Siv Midtun Hollup <siv.hollup@gmail.com>
2019-02-28 09:29:23 +0100

    Removed old comments from PngConverter main method

--- v19/design-patterns-12/demo/src/main/java/inf112/patterns/factory/ToPNGConverter.java
+++ v19/design-patterns-12/demo/src/main/java/inf112/patterns/factory/ToPNGConverter.java
@@ -13,34 +13,17 @@ import inf112.patterns.factory.writer.ImageWriterFactory;
  */
 public class ToPNGConverter {
 
-    /**
-     *  Et program som bruker biblioteker for å lese og skrive bilder.
-     *  Generelt: ofrer "kjapp" implementasjon med alle detaljer på plass for lesbarhet og riktige abstraksjonsnivå
-     *
-     * @param args
-     */
     public static void main(String[] args) {
         String filename = args[0];
 
-        // Vi bryr oss ikke om hvilken reader vi bruker så lenge den virker. Vi vil helst ikke vite noe om hvilke typer som finnes på dette abstraksjonsnivået
         ImageReader reader = getImageReader(filename);
 
-        // Tradisjonell factoryklasse som gir riktig writer (gitt av bruken i dette programmet). Kunne også vært bestemt utfra brukervalg (i meny e.l.)
-        // NB: Her må vi forholde oss til at det i alle fall finnes en type, selv om vi ikke må forholde oss til konkret implementasjon
         ImageWriter writer = ImageWriterFactory.makePngWriter();
 
-        // Bruker reader og writer som strategier, converteren vet ingenting om hvilke implementasjoner den får
         Converter converter = new Converter(reader, writer);
         converter.convert(filename);
     }
 
-
-    /**
-     * Factory method: bruker filnavn-suffix til å gjette hvilken type fil som skal leses inn
-     *
-     * @param imageFileName
-     * @return riktig imageReader som kan lese filen som er oppgitt
-     */
     private static ImageReader getImageReader(String imageFileName) {
         if (imageFileName.endsWith(".gif")) {
             return new GifReader(imageFileName);
